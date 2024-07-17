87ede3f8bbd523514959ade94620044eba02fd46
Armeldakayao <lunixdakayao11@gmail.com>
2024-07-17 18:50:24 +0100

    fix/bugs

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import AuthBgSvg from '../components/ui/auth-bg-svg'
 export default function StepThreePage() {
   return (
     <div className="grid grid-cols-10">
-      <div className="relative z-10  col-span-5 h-screen bg-[linear-gradient(#A8D3722E,#102A5B7A),url(/images/fond.svg)] bg-cover bg-no-repeat"></div>
+      <div className="relative z-10  col-span-5 h-screen  bg-[linear-gradient(#A8D3722E,#102A5B7A),url(/images/fond.svg)] bg-cover bg-no-repeat"></div>
       <div className="relative col-span-5 bg-white  flex h-screen items-center">
         <div className="mx-auto mb-5 space-y-4">
           <div className="bg-white ">
